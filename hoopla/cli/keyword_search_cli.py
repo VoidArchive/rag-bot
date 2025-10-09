ce7b1c47020f908b9b393c9354c8eec13751a0b6
void <voidarchived@gmail.com>
2025-10-09 11:56:43 +0545

    refactored code according to bootdev

--- hoopla/cli/keyword_search_cli.py
+++ hoopla/cli/keyword_search_cli.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
-import json
-import string
-from nltk.stem import PorterStemmer
+
+from lib.keyword_search import (
+    search_command,
+)
 
 
 def main() -> None:
@@ -17,49 +18,13 @@ def main() -> None:
 
     match args.command:
         case "search":
-            search_movies(args.query)
+            print(f"Searching for: {args.query}")
+            results = search_command(args.query)
+            for i, res in enumerate(results, start=1):
+                print(f"{i}, {res['title']}")
 
         case _:
             parser.print_help()
-
-
-def search_movies(query: str) -> None:
-    with open("data/movies.json", "r") as f:
-        data = json.load(f)
-
-    with open("data/stopwords.txt", "r") as f:
-        stop_words = f.read().splitlines()
-
-    translator = str.maketrans("", "", string.punctuation)
-    stemmer = PorterStemmer()
-    query_normalized = query.lower().translate(translator)
-    query_tokens = [
-        stemmer.stem(token)
-        for token in query_normalized.split()
-        if token and token not in stop_words
-    ]
-
-    results = []
-    for movie in data["movies"]:
-        title_normalized = movie["title"].lower().translate(translator)
-        title_tokens = [
-            stemmer.stem(token)
-            for token in title_normalized.split()
-            if token and token not in stop_words
-        ]
-        if any(
-            query_token in title_token
-            for query_token in query_tokens
-            for title_token in title_tokens
-        ):
-            results.append(movie)
-
-    results.sort(key=lambda m: m["id"])
-    results = results[:5]
-
-    print(f"Searching for: {query}")
-    for i, movie in enumerate(results, start=1):
-        print(f"{i}, {movie['title']}")
 
 
 if __name__ == "__main__":
